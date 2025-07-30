78d6b603e0a519463bd83ae482b1f32461734742
dans6887 <dans6887@gmail.com>
2025-07-30 13:11:05 -0400

    Create RestartServices.py

---
+++ Switch/RestartServices.py
@@ -0,0 +1,29 @@
+import subprocess
+
+SERVICE_NAME = "parkplacetech-agent-updater"
+SERVICE_NAME2 = "parkplacetech-agent-updater"
+
+def run_command(command):
+    try:
+        print(f"Running: {' '.join(command)}")
+        subprocess.run(command, check=True)
+        print(f"Command {' '.join(command)} executed successfully.\n")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command {' '.join(command)}: {e}\n")
+
+def stop_service():
+    run_command(["systemctl", "stop", SERVICE_NAME])
+    run_command(["systemctl", "stop", SERVICE_NAME2])
+    
+def start_service():
+    run_command(["systemctl", "start", SERVICE_NAME])
+    run_command(["systemctl", "start", SERVICE_NAME2])
+
+if __name__ == "__main__":
+    print(f"Stopping service: {SERVICE_NAME} and {SERVICE_NAME2}")
+    print()
+    stop_service()
+
+    print(f"Starting service: {SERVICE_NAME} and {SERVICE_NAME2}")
+    print()
+    start_service()
